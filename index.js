10294256d1eb582f7b56ec696e3e9022e289b7e6
Siddeshwar Gauncar <sddshwr@gmail.com>
2018-07-18 18:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ function happyHolidaysTo(name) {
 }
 happyHolidaysTo(name);
 function happyHolidayTo(holiday, name) {
+<<<<<<< HEAD
   return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days) {
+=======
+  return `${holiday}, ${name}!`;
+}
+
+function holidayCountdown(days, holiday) {
+>>>>>>> 80b02499d25e179fda3dc21e63436125d3f1e6e9
   return `It\'s ${days} days until ${holiday}!`;
 }
